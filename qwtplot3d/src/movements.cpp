e0be050df2c9bea36fc26ddecc9703639f648c3e
krischnamurti <krischnamurti@97aaf149-a00d-0410-8b2c-f8ddddd916e9>
2003-05-03 09:15:13 +0000

    small fix

--- qwtplot3d/src/movements.cpp
+++ qwtplot3d/src/movements.cpp
@@ -6,8 +6,12 @@
 #include "qwt_plot3d.h"
 
 
-/*!
-  Set the rotation angle of the object to \e degrees around the X axis.
+/**
+  Set the rotation angle of the object. If you look along the respective axis towards ascending values,
+	the rotation is performed in mathematical \e negative sense 
+	\param xVal angle in \e degree to rotate around the X axis
+	\param yVal angle in \e degree to rotate around the Y axis
+	\param zVal angle in \e degree to rotate around the Z axis
 */
 void 
 QwtPlot3D::setRotation( double xVal, double yVal, double zVal )
@@ -23,6 +27,13 @@ QwtPlot3D::setRotation( double xVal, double yVal, double zVal )
 	emit rotationChanged(xVal, yVal, zVal);
 }
 
+/**
+  Set the shift in object (world) coordinates.
+	\param xVal shift along (world) X axis
+	\param yVal shift along (world) Y axis
+	\param zVal shift along (world) Z axis
+	\see setViewportShift()
+*/
 void 
 QwtPlot3D::setShift( double xVal, double yVal, double zVal )
 {
@@ -36,23 +47,34 @@ QwtPlot3D::setShift( double xVal, double yVal, double zVal )
 	emit shiftChanged(xVal, yVal, zVal);
 }
 
+/**
+  Performs shifting along screen axes. The values are limited to the interval [-1..1]
+	The shift moves points inside a sphere, which encloses the unscaled and unzoomed data
+	by multiples of the spheres diameter
+	
+	\param xVal shift along (view) X axis
+	\param yVal shift along (view) Y axis
+	\see setShift()
+*/
 void 
 QwtPlot3D::setViewportShift( double xVal, double yVal )
 {
   if (xVPShift_ == xVal && yVPShift_ == yVal)
 		return;
 	
-	if (xVal < -2)
-		xVPShift_ = -2;
-	else if (xVal > 2)
-		xVPShift_ = 2;
+	double limit = 1;
+
+	if (xVal < -limit)
+		xVPShift_ = -limit;
+	else if (xVal > limit)
+		xVPShift_ = limit;
 	else
 		xVPShift_ = xVal;
 
-	if (yVal < -2)
-		yVPShift_ = -2;
-	else if (yVal > 2)
-		yVPShift_ = 2;
+	if (yVal < -limit)
+		yVPShift_ = -limit;
+	else if (yVal > limit)
+		yVPShift_ = limit;
 	else
 		yVPShift_ = yVal;
 		
@@ -60,6 +82,14 @@ QwtPlot3D::setViewportShift( double xVal, double yVal )
 	emit vieportShiftChanged(xVPShift_, yVPShift_);
 }
 
+/**
+  Set the scale in object (world) coordinates.
+	\param xVal scaling for X values
+	\param yVal scaling for Y values
+	\param zVal scaling for Z values 
+
+	A respective value of 1 represents no scaling;
+*/
 void 
 QwtPlot3D::setScale( double xVal, double yVal, double zVal )
 {
@@ -73,6 +103,10 @@ QwtPlot3D::setScale( double xVal, double yVal, double zVal )
 	emit scaleChanged(xVal, yVal, zVal);
 }
 
+/**
+  Set the (zoom in addition to scale).
+	\param val zoom value (value == 1 indicates no zooming)
+*/
 void 
 QwtPlot3D::setZoom( double val )
 {
