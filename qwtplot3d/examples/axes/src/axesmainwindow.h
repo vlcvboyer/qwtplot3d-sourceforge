ca1b29acadd8ca7b152f0725ca68f491728e3bc2
krischnamurti <krischnamurti@97aaf149-a00d-0410-8b2c-f8ddddd916e9>
2004-05-25 18:10:01 +0000

    misc new headers and src files, new example axis, removed example print (functionality in mesh2),

---
+++ qwtplot3d/examples/axes/src/axesmainwindow.h
@@ -0,0 +1,34 @@
+#include "qwt3d_surfaceplot.h"
+#include "qwt3d_function.h"
+#include "qwt3d_plot.h"
+#include "axesmainwindowbase.h"
+
+
+class AxesMainWindow : public AxesMainWindowBase
+{
+	Q_OBJECT
+
+public:
+	AxesMainWindow( QWidget* parent = 0, const char* name = 0, WFlags f = WType_TopLevel );
+	~AxesMainWindow();
+	Qwt3D::SurfacePlot* plot;
+	Qwt3D::Function *rosenbrock;
+  void resetTics();
+
+public slots:
+	void setNumberGap(int gap);
+	void setLabelGap(int gap);
+
+  void setSmoothLines(bool);
+  void setTicLength(int val);
+  void setTicNumber(int degree);
+
+  void standardItems();
+  void complexItems();
+  void letterItems();
+  void timeItems();
+
+private:
+
+  int tics;
+};
