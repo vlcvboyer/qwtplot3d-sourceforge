18407218f2aeb4c9d628b08cbd924f6bb7918bf7
krischnamurti <krischnamurti@97aaf149-a00d-0410-8b2c-f8ddddd916e9>
2004-06-08 00:04:39 +0000

    all files are now in a directory qwtplot3d

--- qwtplot3d/scripts/makedocu.py
+++ qwtplot3d/scripts/makedocu.py
@@ -8,7 +8,8 @@ from time import gmtime, strftime
 refuseddirs = ['CVS','images']
 refuseddirsextra = ['CVS','small']
 
-source = 'V:\\\\cvs\\uploads\\qwtplot3d\\'
+source = 'V:\\\\cvs\\qwtplot3d\\doc\\'
+sourceim = 'V:\\\\cvs\\qwtplot3d\\doc\\images\\'
 zipname = 'V:\\\\cvs\\uploads\\qwtplot3d-doc.zip'
 zipextraname = 'V:\\\\cvs\\uploads\\qwtplot3d-doc-extra.zip'
 
@@ -23,13 +24,13 @@ def compresstree(src, zip, refdir):
             if name == 'images':    
                 compresstree(srcname +'\\small', zip, refdir)
         else:
-            zip.write(srcname, srcname[len(source):])
+            zip.write(srcname,'qwtplot3d\\doc\\' +  srcname[len(source):])
                 
 
 
 zip = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
-compresstree(source +'\\doc\\', zip, refuseddirs)
+compresstree(source, zip, refuseddirs)
 zip.close()
 zip = zipfile.ZipFile(zipextraname, 'w', zipfile.ZIP_DEFLATED)
-compresstree(source +'\\doc\\images\\', zip, refuseddirsextra)
+compresstree(sourceim, zip, refuseddirsextra)
 zip.close()
