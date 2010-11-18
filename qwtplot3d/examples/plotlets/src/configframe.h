340d1ec917f0e0a6b729a7caedd97fa1ae08af55
krischnamurti <krischnamurti@97aaf149-a00d-0410-8b2c-f8ddddd916e9>
2010-11-18 14:10:38 +0000

    Examples/Plotlets: individual appearance works (except normals)

--- qwtplot3d/examples/plotlets/src/configframe.h
+++ qwtplot3d/examples/plotlets/src/configframe.h
@@ -2,7 +2,8 @@
 #ifndef configdialog_h__2010_02_18_14_05_33_begin_guarded_code
 #define configdialog_h__2010_02_18_14_05_33_begin_guarded_code
 
-#include <qapplication.h>
+#include <qsignalmapper.h>
+#include "qwt3d_color_std.h"
 
 #include "ui_configframebase.h"
 
@@ -13,9 +14,28 @@ class ConfigFrame : public QFrame, protected Ui::ConfigFrameBase
     
 public:
   ConfigFrame(QWidget *parent=0);
+  Qwt3D::RGBA meshColor() const {return meshcolor_;}
+  const Qwt3D::StandardColor& dataColor() const {return datacolor_;}
+  Qwt3D::PLOTSTYLE plotstyle() const {return plotstyle_;}
 
-public slots:
-	void setEmission(int);
+signals:
+  void meshColorChanged();
+  void dataColorChanged();
+  void plotStyleChanged();
+
+private slots:
+  void slotMeshColor();
+  void slotDataColor();
+  void slotPlotstyle(const QString& val);
+
+private:
+  QSignalMapper *signalmapper_;
+  Qwt3D::RGBA meshcolor_;
+  Qwt3D::StandardColor datacolor_;
+  Qwt3D::PLOTSTYLE plotstyle_;
+
+  bool openColorMap(Qwt3D::ColorVector& cv, QString fname);
+
 };
 
 #endif /* include guarded */
