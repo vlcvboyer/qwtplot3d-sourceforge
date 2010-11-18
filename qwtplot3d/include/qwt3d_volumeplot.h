97efd59817ebc027879ad943a8962398d897bf8c
krischnamurti <krischnamurti@97aaf149-a00d-0410-8b2c-f8ddddd916e9>
2010-11-18 13:41:50 +0000

    VolumePlot excluded

--- qwtplot3d/include/qwt3d_volumeplot.h
+++ qwtplot3d/include/qwt3d_volumeplot.h
@@ -2,20 +2,20 @@
 #ifndef qwt3d_volumeplot_h__2004_03_06_01_52_begin_guarded_code
 #define qwt3d_volumeplot_h__2004_03_06_01_52_begin_guarded_code
 
-#include "qwt3d_plot3d.h"
-
-namespace Qwt3D
-{
-
-//! TODO
-class QWT3D_EXPORT VolumePlot : public Plot3D
-{
-//    Q_OBJECT
-
-public:
-  VolumePlot( QWidget* parent = 0, const char* name = 0 ){}
-};
-
-} // ns
+//#include "qwt3d_plot3d.h"
+//
+//namespace Qwt3D
+//{
+//
+////! TODO
+//class QWT3D_EXPORT VolumePlot : public Plot3D
+//{
+////    Q_OBJECT
+//
+//public:
+//  VolumePlot( QWidget* parent = 0, const char* name = 0 ){}
+//};
+//
+//} // ns
 
 #endif /* include guard */
